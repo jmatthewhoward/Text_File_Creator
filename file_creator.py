56e89c45d2253399c37094050844aec172ca9846
jmatthewhoward <josephmatthewhoward@gmail.com>
2025-06-09 21:21:50 -0400

    added favorite color {blue}

--- file_creator.py
+++ file_creator.py
@@ -2,3 +2,4 @@ Joe = input("What's your name? \n")
 color = input("What's your favorite color? /n")
 with open('example.txt', 'w') as file:
     file.write(f"{Joe} created this file")
+    file.write(f"Their favorite color is {Blue}")
