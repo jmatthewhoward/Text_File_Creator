06abf0898e7e01c3ab4af2d20bd4ec8b5380c377
jmatthewhoward <josephmatthewhoward@gmail.com>
2025-06-09 21:29:22 -0400

    updated syntax errors

--- file_creator.py
+++ file_creator.py
@@ -1,5 +1,5 @@
 Joe = input("What's your name? \n")
 color = input("What's your favorite color? \n")
 with open('example.txt', 'w') as file:
-    file.write(f"{Joe} created this file")
-    file.write(f"Their favorite color is {blue}")
+    file.write(f"{Joe} created this file\n")
+    file.write(f"Their favorite color is {color}")
